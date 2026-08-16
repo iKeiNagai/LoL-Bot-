0e7c4a26b3e4d10c33496035f1028200497b486f
Thomas <thomasvasquez68@gmail.com>
2026-08-15 21:24:30 -0400

    feat: commands command

--- components/__init__.py
+++ components/__init__.py
@@ -1,6 +1,8 @@
 from .hello import HelloComponent
+from .league import LeagueComponent
 
 # import all components here 
 __all__ = (
     "HelloComponent",
+    "LeagueComponent"
 )
--- components/hello.py
+++ components/hello.py
@@ -7,3 +7,12 @@ class HelloComponent(commands.Component):
     @commands.command(name="hello")
     async def hello(self, ctx: commands.Context):
         await ctx.send(f"Hello, {ctx.author.name}!")
+
+    @commands.command()
+    async def commands(self, ctx: commands.Context):
+        commands = [
+             "!hello",
+             "!lurk"
+        ]
+
+        await ctx.send(f"{' • '.join(commands)}")
